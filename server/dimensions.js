eac4a9689669db870ba2ce43b463b706ec7a3b76
Christopher Roussel <christopher@impleri.net>
2023-03-23 11:01:15 -0500

    update dimension skills in 1.18

--- server/dimensions.js
+++ server/dimensions.js
@@ -1,17 +1,12 @@
-// // Test catching all teleport events, plus event.entity
-// DimensionEvents.onTeleport((event) => {
-//   console.info(
-//     `Player ${event.entity.name} trying to teleport to ${event.destination}`
-//   );
-//   logSkills(skillsFor(event.entity).all, "[DIMENSION]");
-// });
-//
-// // Test general blocking (should not be able to teleport at all)
-// DimensionEvents.onTeleport("minecraft:the_nether", (event) => event.cancel());
-//
-// // Test skills-based blocking plus event.player and event.deny (should not be able to teleport)
-// DimensionEvents.onTeleport("minecraft:the_end", (event) => {
-//   if (skillsFor(event.player).cannot(STARTED_QUEST)) {
-//     event.deny();
-//   }
-// });
+// Test catching all teleport events, plus event.entity
+onEvent("dimensionEvents.register", (event) => {
+  event.restrict("the_end", (r) =>
+    r.inaccessible().if((player) => player.cannot(STARTED_QUEST))
+  );
+
+  event.restrict("minecraft:the_nether", (r) =>
+    r.replaceWith("the_end").if(hasntKillCount(5))
+  );
+
+  event.restrict("the_nether", (r) => r.inaccessible().unless(hasKillCount(8)));
+});
